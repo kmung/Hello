ef37d2f5c4492bb83dc0ba8675c13297dd2add5e
kmung <46796559+kmung@users.noreply.github.com>
2019-01-17 15:03:15 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: David Scalzo, Khawm Mung
+ * Date: 2019/15/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("David Scalzo, Khawm Mung\n");
+
+  return 0;
+}
